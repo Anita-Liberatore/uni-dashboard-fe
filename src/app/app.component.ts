cc7bcec35bfbce74f161f0999a40a8a3e6c4e533
Anita Liberatore <anitaliberatore@hotmail.com>
2025-03-29 00:59:25 +0100

    Initial commit - Angular 19 + Tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'uni-dashboard-fe';
