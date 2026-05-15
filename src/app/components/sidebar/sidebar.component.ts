1f2e0f98f0d393b3900314591ebfe4ec0cf504e0
GBSHY081 <anitaliberatore@hotmail.com>
2026-05-15 14:40:57 +0200

    fix initials name

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -18,6 +18,7 @@ export class SidebarComponent {
   /** Logged-in student shown in the footer */
   readonly student = {
     name: 'Anita Liberatore',
+    initials: 'AL',
     id: 'S1234567',
   };
 
